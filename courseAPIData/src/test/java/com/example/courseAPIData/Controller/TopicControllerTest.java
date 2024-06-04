9f7e0964cc0960704abf79fe89484ad0bc52c52f
Rebecca Zipper <rebecca.zipper@gmail.com>
2024-06-04 12:51:47 -0400

    Update TopicControllerTest.java

--- courseAPIData/src/test/java/com/example/courseAPIData/Controller/TopicControllerTest.java
+++ courseAPIData/src/test/java/com/example/courseAPIData/Controller/TopicControllerTest.java
@@ -1,23 +1,28 @@
 package com.example.courseAPIData.Controller;
 
 import com.example.courseAPIData.Model.TopicModel;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -57,9 +62,15 @@ class TopicControllerTest {
     @Test
     void addTopic() throws Exception {
         TopicModel topic = new TopicModel("Rails", "Coding Framework: Rails", "Description");
-        mvc.perform(get("/topics").accept(MediaType.APPLICATION_JSON))
+
+        mvc.perform(post("/topics")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(new ObjectMapper().writeValueAsString(topic)))
+                // Expect the response status to be OK
                 .andExpect(status().isOk())
-                .andReturn();
+                // Print the response content for debugging purposes
+                .andDo(print());
 
         List<TopicModel> topics = new ArrayList<>(Arrays.asList(
                 new TopicModel("Spring", "Coding Framework: Spring", "Description"),
@@ -82,24 +93,20 @@ class TopicControllerTest {
 
     @Test
     void updateTopic() throws Exception {
-        List<TopicModel> topics = new ArrayList<>(Arrays.asList(
-                new TopicModel("Spring", "Coding Framework: Spring", "Description"),
-                new TopicModel("Java", "Coding Language: Java", "Description"),
-                new TopicModel("Python", "Coding Language: Python", "Description")
-        ));
+        TopicModel topic = new TopicModel("1", "Java", "Programming Language");
+        doNothing().when(topicController).updateTopic(topic,topic.getId());
+//        when(topicController.updateTopic(topic, topic.getId())).thenReturn(topic);
 
-        topics.getFirst().setName("Spring1");
-        mvc.perform(get("/topics/{id}", "Spring").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
+        mvc.perform(put("/topics/{id}", "1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":\"1\",\"name\":\"Python\",\"description\":\"Programming Language\"}"))
+                .andExpect(status().isOk());
 
-        given(topicController.getAllTopics()).willReturn(topics);
-        mvc.perform(get("/topics").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id", is("Spring")))
-                .andExpect(jsonPath("$[0].name", is("Spring1")))
-                .andDo(print());
+//        List<TopicModel> topics = topicController.getAllTopics();
+//        assertEquals(1, topics.size());
+//        assertEquals("Java", topics.getFirst().getName());
     }
+
 
     @Test
     void deleteTopic() throws Exception {
@@ -108,17 +115,13 @@ class TopicControllerTest {
                 new TopicModel("Java", "Coding Language: Java", "Description"),
                 new TopicModel("Python", "Coding Language: Python", "Description")
         ));
-
-        topics.removeFirst();
-
-        mvc.perform(get("/topics/{id}", "Spring").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-
         given(topicController.getAllTopics()).willReturn(topics);
+
         mvc.perform(get("/topics").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(print());
+
+        mvc.perform(delete("/topics/{id}", "Spring").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
                 .andDo(print());
     }
 }
